59bdeec9b57477e5cb23fb6f88974c1a302b986f
shabinamirajkar <shabee@gmail.com>
2017-08-22 14:37:08 -0700

    SM

---
+++ eTimeWeb/Scripts/CommonScripts.js
@@ -0,0 +1,11 @@
+﻿
+function getParameterByName(name) {
+    var regexS = "[\\?&]" + name + "=([^&#]*)",
+  regex = new RegExp(regexS),
+  results = regex.exec(window.location.search);
+    if (results == null) {
+        return "";
+    } else {
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+}
